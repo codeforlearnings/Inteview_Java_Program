2e6d7c2696a5b0aa81589d0f554f49ab72172a3d
Pankaj Kumar <Pankaj Kumar@Universe>
2023-05-20 23:00:23 +0530

    1st commit

---
+++ Interview_Java_Program/src/main/java/com/example/demo/InterviewJavaProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewJavaProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewJavaProgramApplication.class, args);
+	}
+
+}
