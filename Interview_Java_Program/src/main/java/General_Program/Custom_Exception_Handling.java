550927111656a1e5c7652e4ad7992ecf35c87678
Pankaj Kumar <Pankaj Kumar@Universe>
2023-08-01 00:46:46 +0530

    2nd commit

--- Interview_Java_Program/src/main/java/General_Program/Custom_Exception_Handling.java
+++ Interview_Java_Program/src/main/java/General_Program/Custom_Exception_Handling.java
@@ -1,5 +1,31 @@
 package General_Program;
 
-public class Custom_Exception_Handling {
+ class Custom_Exception extends Exception {
+	// A Class that represents use-defined exception
+	
+	    public Custom_Exception(String s)
+	    {
+	        // Call constructor of parent Exception
+	        super(s);
+	    }
+	}
+	 
+	// A Class that uses above MyException
+	public class Custom_Exception_Handling {
+	    // Driver Program
+	    public static void main(String args[])
+	    {
+	        try {
+	            // Throw an object of user defined exception
+	            throw new Custom_Exception("GeeksGeeks");
+	        }
+	        catch (Custom_Exception ex) {
+	            System.out.println("Caught");
+	 
+	            // Print the message from MyException object
+	            System.out.println(ex.getMessage());
+	        }
+	    }
+	}
 
-}
+
--- Interview_Java_Program/src/main/java/General_Program/MultiThreading.java
+++ Interview_Java_Program/src/main/java/General_Program/MultiThreading.java
@@ -1,5 +1,75 @@
 package General_Program;
 
-public class MultiThreading {
+class MyThread extends Thread{
+	public void run() {
+		Thread th1=Thread.currentThread();
+		//Thread th2=Thread.currentThread();
+		//Thread th3=Thread.currentThread();
+		ThreadGroup tg=th1.getThreadGroup();
+		for(int i=1;i<=10;i++) {
+		System.out.println(th1.getName()+"-"+tg.getName()+"-"+th1.getState());
+	//	System.out.println(th2.getName()+"-"+th2.getState());
+	//	System.out.println(th3.getName()+"-"+th3.getState());
+		
+		
+		try {
+		//	wait(500);
+			//Thread.sleep(500);
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+				
+		}
+	}
+}
 
+
+
+public class MultiThreading {
+	public static void main(String[] args) throws Throwable {
+		MyThread th=new MyThread();
+		Thread t1=new Thread(th);
+		Thread t2=new Thread(th);
+		Thread t3=new Thread(th);
+		System.out.println();
+		t1.start();
+		
+		try {
+			t1.join(1000);
+				
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		
+		try {
+			//	wait(500);
+				Thread.sleep(500);
+				t2.join(1000);
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		
+		try {
+			//	wait(500);
+				Thread.sleep(2000);
+				t3.join(1000);
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			}
 }
